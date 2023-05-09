111344752a5a8045abf3fb5a0b016043ec75b055
Kamay Xutax <admin@xutaxkamay.com>
2023-05-09 16:55:59 +0200

    More extensive tests

--- src/simulator/Test.cpp
+++ src/simulator/Test.cpp
@@ -17,25 +17,39 @@ int main()
 {
     std::cout << "Serializing test ...\n";
 
-    LogicGate::Serializer logicGateInverterSerializer;
-    logicGateInverterSerializer.input_ports       = { 0_p };
-    logicGateInverterSerializer.output_ports      = { 0_p };
-    logicGateInverterSerializer.input_truth_table = {
-        { 0_b },
-        { 1_b },
-    };
+    std::vector<LogicGate::Serializer> logicGatesSerializer;
 
-    logicGateInverterSerializer.output_truth_table = { { 1_b }, { 0_b } };
+    logicGatesSerializer.push_back({
+      { 0_p },
+      { 1_p },
+      { { 0_b }, { 1_b } },
+      { { 1_b }, { 0_b } }
+    });
 
-    FPGA::Serializer fpgaSerializer { 1,
-                                      { logicGateInverterSerializer } };
+    for (std::size_t index = 2; index < 600002; index += 2)
+    {
+        logicGatesSerializer.push_back({
+          { index },
+          { index + 1 },
+          { { 0_b }, { 1_b } },
+          { { 1_b }, { 0_b } }
+        });
+
+        logicGatesSerializer.push_back({
+          { index + 1 },
+          { index },
+          { { 0_b }, { 1_b } },
+          { { 1_b }, { 0_b } }
+        });
+    }
+
+    FPGA::Serializer fpgaSerializer { 600002, logicGatesSerializer };
 
     FPGA::Deserializer fpgaDeserializer;
     auto fpga = fpgaDeserializer.Deserialize(fpgaSerializer.Serialize());
 
+    fpga.GetPort(0)->SetLow();
     std::cout << "FPGA test ... \n";
-
-    fpga.PrepareStages();
 
     Timer timerFPGA;
     Timer timerPrint;
@@ -43,6 +57,7 @@ int main()
 
     std::int64_t averageTime      = 0;
     std::int64_t countAverageTime = 0;
+    fpga.PrepareStages();
 
     while (true)
     {
@@ -59,6 +74,9 @@ int main()
                       << " nanoseconds of average time in one second\n";
             countAverageTime = 0;
             averageTime      = 0;
+
+            std::cout << "port 1 should have state 1: "
+                      << fpga.GetPort(1)->state << '\n';
 
             timerPrint.Start();
         }
