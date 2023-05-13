2ff4ac7632526fc92e79de03d85083597a5c2c99
Kamay Xutax <admin@xutaxkamay.com>
2023-05-13 04:18:25 +0200

    Removed non-used std

--- src/library/StandardHeaders.h
+++ src/library/StandardHeaders.h
@@ -1,18 +1,9 @@
 #ifndef VFPGA_STANDARD_HEADERS_H
 #define VFPGA_STANDARD_HEADERS_H
 
-#include <algorithm>
-#include <bit>
-#include <bitset>
-#include <condition_variable>
-#include <cstdint>
 #include <execution>
-#include <functional>
-#include <future>
 #include <iostream>
-#include <memory>
 #include <ranges>
-#include <semaphore>
 #include <variant>
 #include <vector>
 
